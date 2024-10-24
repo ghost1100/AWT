9c1b3941a01d3d38a694c193f271a4337f8e0bdf
Ahmed Mursal <92726128+ghost1100@users.noreply.github.com>
2024-10-24 20:13:02 +0100

    Add files via upload

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # pip install flask
 from flask import Flask, render_template, request, redirect, url_for, flash
 
@@ -8,4 +9,29 @@ def home():
     return "Hello, Flask!"
 
 if __name__ == '__main__':
+=======
+# pip install flask
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template ('Home.html')
+
+
+@app.route('/splashscreen')
+def splash_screen():
+    return render_template('SplashScreen.html') 
+
+@app.route('/todo')  
+def todo():
+    return render_template('ToDo.html')  
+
+@app.route('/weather') 
+def weather():
+    return render_template('Weather.html')
+
+if __name__ == '__main__':
+>>>>>>> Stashed changes
     app.run(debug=True)
