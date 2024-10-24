d4bd712c5859e13299da3e658cb2d4ee2610858e
Ahmed Mursal <92726128+ghost1100@users.noreply.github.com>
2024-10-24 19:50:06 +0100

    Add files via upload

---
+++ App.py
@@ -0,0 +1,11 @@
+# pip install flask
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
