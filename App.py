045385962a051f42e30e279efd6e0ec0436cf4f8
Ahmed Mursal <92726128+ghost1100@users.noreply.github.com>
2024-11-14 00:20:13 +0000

    Update App.py

--- App.py
+++ App.py
@@ -1,11 +1,24 @@
 # pip install flask
 from flask import Flask, render_template, request, redirect, url_for, flash
 
+
 app = Flask(__name__)
 
 @app.route('/')
+def splash_screen():
+    return render_template('SplashScreen.html') 
+
+@app.route('/home')
 def home():
-    return "Hello, Flask!"
+    return render_template ('Home.html')
+
+@app.route('/todo')  
+def todo():
+    return render_template('ToDo.html')  
+
+@app.route('/weather') 
+def weather():
+    return render_template('Weather.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
