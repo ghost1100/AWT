f8bc1b0154be1214575406fc28ff20874d4a2e99
Ahmed Mursal <amursal908@gmail.com>
2024-11-14 00:54:32 +0000

    Added the api key for unsplash

--- App.py
+++ App.py
@@ -1,5 +1,12 @@
 # pip install flask
 from flask import Flask, render_template, request, redirect, url_for, flash
+##API KEYS##
+#Unsplash API key
+UNSPLASH_API_KEY = "nLfSlOoclheYYtRhGHZi5FIBixRMjTJe7Ra6BsVbKEg"
+#Unsplash API URL
+UNSPLASH_API_URL = "https://api.unsplash.com/photos/random"
+
+
 
 app = Flask(__name__)
 
@@ -19,6 +26,18 @@ def todo():
 @app.route('/weather') 
 def weather():
     return render_template('Weather.html')
+
+def get_random_image():
+    query = request.args.get("query", "Nature")
+
+    response = request.get(
+        UNSPLASH_API_URL,
+        headers={"Authorization": f"Bearer {UNSPLASH_API_KEY}"},
+        params={"query": query,"orinetation":"landscape"}
+    )
+
+    if response.status_code == 200:
+        return response.json()
 
 if __name__ == '__main__':
     app.run(debug=True)
