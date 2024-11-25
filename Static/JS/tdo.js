e922fb5d6315e7886e67525599f0b4ee0a98da85
Ahmed Mursal <amursal908@gmail.com>
2024-11-25 05:36:54 +0000

    nothing works again lol am fked...

--- Static/JS/tdo.js
+++ Static/JS/tdo.js
@@ -5,9 +5,35 @@ function addTask(){
     if (inputBox.value ===''){
         alert("Please enter a task");
 
-    }else{
+    }
+    else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = "";
+    saveData();
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName == "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName == "SPAN"){
+  e.target.parentElement.remove();
+  saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+//in this case instead of making one function and calling it save data we will have to make three one to delete the data from db one to save it, one to check it and one to uncheck it so four not three.
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
